af1024bd72d571e90722bf7465bef837dedf40b6
lhasanovic <lhasanovic1@etf.unsa.ba>
2020-08-12 11:43:17 +0200

    changing child activity for teacher panel

---
+++ src/ba/unsa/etf/rpr/SetActivityController.java
@@ -0,0 +1,45 @@
+package ba.unsa.etf.rpr;
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class SetActivityController {
+	public Label childLabel;
+	public ChoiceBox<ChildActivity> chooseActivityBox;
+
+	private Child child;
+
+	public SetActivityController(Child child) {
+		this.child = child;
+	}
+
+	@FXML
+	public void initialize() {
+		chooseActivityBox.getItems().setAll(ChildActivity.values());
+		chooseActivityBox.getSelectionModel().selectFirst();
+		childLabel.setText(child.getFirstName() + " new activity:");
+	}
+
+	public void actionOk(ActionEvent actionEvent) {
+		child.setActivity(chooseActivityBox.getValue());
+
+		Stage stage = (Stage) childLabel.getScene().getWindow();
+		stage.close();
+	}
+
+	public void actionCancel(ActionEvent actionEvent) {
+		child = null;
+
+		Stage stage = (Stage) childLabel.getScene().getWindow();
+		stage.close();
+	}
+
+
+	public Child getChild() {
+		return child;
+	}
+}
--- src/ba/unsa/etf/rpr/TeacherController.java
+++ src/ba/unsa/etf/rpr/TeacherController.java
@@ -3,12 +3,20 @@ package ba.unsa.etf.rpr;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class TeacherController {
 	public TableView<Child> tableViewChildren;
@@ -31,5 +39,34 @@ public class TeacherController {
 		colChildLastName.setCellValueFactory(new PropertyValueFactory("lastName"));
 		colChildBirth.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getDateOfBirthString()));
 		colChildActivity.setCellValueFactory(new PropertyValueFactory("activity"));
+	}
+
+	public void actionSetActivity(ActionEvent actionEvent) {
+		Child child = tableViewChildren.getSelectionModel().getSelectedItem();
+		if(child == null)
+			return;
+
+		Stage stage = new Stage();
+		Parent root = null;
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/set_activity.fxml"));
+			SetActivityController setActivityController = new SetActivityController(child);
+			loader.setController(setActivityController);
+			root = loader.load();
+			stage.setTitle("Change Activity");
+			stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+			stage.setResizable(false);
+			stage.show();
+
+			stage.setOnHiding( event -> {
+				Child editedChild = setActivityController.getChild();
+				if (editedChild != null) {
+					child.setActivity(editedChild.getActivity());
+					tableViewChildren.refresh();
+				}
+			} );
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
