893d6e335d4fac2887cb9c2972a61311bb533e33
lhasanovic <lhasanovic1@etf.unsa.ba>
2020-08-12 14:01:53 +0200

    initialization and validation of fields in add_diary

--- src/ba/unsa/etf/rpr/AddDiaryEntryController.java
+++ src/ba/unsa/etf/rpr/AddDiaryEntryController.java
@@ -3,10 +3,13 @@ package ba.unsa.etf.rpr;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.util.Duration;
+import org.controlsfx.control.Notifications;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -30,10 +33,12 @@ public class AddDiaryEntryController {
 	@FXML
 	public void initialize() {
 		childBox.setItems(children);
+		childBox.getSelectionModel().select(child);
 		diaryDatePicker.setValue(LocalDate.now());
 		hourField.setText("" + LocalDateTime.now().getHour());
 		minuteField.setText("" + LocalDateTime.now().getMinute());
 		childActivityBox.getItems().setAll(ChildActivity.values());
+		childActivityBox.getSelectionModel().select(child.getActivity());
 
 		hourField.textProperty().addListener((obs, oldVal, newVal) -> {
 			if(matchesHourFormat(newVal)) {
@@ -57,10 +62,22 @@ public class AddDiaryEntryController {
 	}
 
 	public void actionOk(ActionEvent actionEvent) {
+		if(allGood()) {
+			if(diaryDatePicker.getValue().isAfter(LocalDate.now())) {
+				String title = "Invalid date!";
+				String text = "Date can't be in the future";
+				notify(title, text);
+			} else if(diaryDatePicker.getValue().isBefore(child.getDateOfBirth())) {
+				String title = "Invalid date!";
+				String text = "Date can't be older than the child!";
+				notify(title, text);
+			} else {
 
+			}
+		}
 	}
 
-	public boolean matchesHourFormat(String s) {
+	private boolean matchesHourFormat(String s) {
 		boolean matches =  s != null &&
 				s.length() != 0 &&
 				s.matches("^[0-9]{1,2}?$");
@@ -73,7 +90,7 @@ public class AddDiaryEntryController {
 		return matches && hours >= 0 && hours <= 23;
 	}
 
-	public boolean matchesMinuteFormat(String s) {
+	private boolean matchesMinuteFormat(String s) {
 		boolean matches =  s != null &&
 				s.length() != 0 &&
 				s.matches("^[0-9]{1,2}?$");
@@ -84,6 +101,19 @@ public class AddDiaryEntryController {
 		int minutes = Integer.parseInt(s);
 
 		return matches && minutes >= 0 && minutes <= 59;
+	}
 
+	private boolean allGood() {
+		return hourField.getStyleClass().contains("fieldRight") &&
+				minuteField.getStyleClass().contains("fieldRight");
+	}
+
+	private void notify(String title, String text) {
+		Notifications notificationBuilder = Notifications.create()
+				.title(title)
+				.text(text)
+				.hideAfter(Duration.seconds(2))
+				.position(Pos.BOTTOM_RIGHT);
+		notificationBuilder.showError();
 	}
 }
