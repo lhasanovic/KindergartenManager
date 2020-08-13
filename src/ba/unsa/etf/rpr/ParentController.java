f09b04130b5351bb3c61190baebccb709f75ccf9
lhasanovic <lhasanovic1@etf.unsa.ba>
2020-08-13 17:32:06 +0200

    parent panel backend completed

--- src/ba/unsa/etf/rpr/ParentController.java
+++ src/ba/unsa/etf/rpr/ParentController.java
@@ -1,15 +1,27 @@
 package ba.unsa.etf.rpr;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class ParentController {
 	private ChildDiary childDiary;
 
 	public ImageView activityImg;
 	public Label activityLabel;
+	public Label teacherFirstNameLabel;
+	public Label teacherLastNameLabel;
+	public Label teacherPhoneNumberLabel;
 
 	public ParentController(ChildDiary childDiary) {
 		this.childDiary = childDiary;
@@ -17,7 +29,27 @@ public class ParentController {
 
 	@FXML
 	public void initialize() {
-		activityImg.setImage(new Image("/img/" + childDiary.getChild().toString() + ".jpg"));
+		activityImg.setImage(new Image("/img/" + childDiary.getChild().getActivity().toString() + ".jpg"));
 		activityLabel.setText("Your child is currently " + childDiary.getChild().getActivity().toString().toLowerCase());
+		teacherFirstNameLabel.setText(childDiary.getChild().getTeacher().getFirstName());
+		teacherLastNameLabel.setText(childDiary.getChild().getTeacher().getLastName());
+		teacherPhoneNumberLabel.setText(childDiary.getChild().getTeacher().getPhoneNumber());
+	}
+
+	public void actionViewDiary(ActionEvent actionEvent) {
+		Stage stage = new Stage();
+		Parent root = null;
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/view_diary_parent.fxml"));
+			ViewDiaryController viewDiaryController = new ViewDiaryController(childDiary);
+			loader.setController(viewDiaryController);
+			root = loader.load();
+			stage.setTitle("View Diary");
+			stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+			stage.setResizable(false);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
